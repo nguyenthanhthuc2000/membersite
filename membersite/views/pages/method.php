1e5a10d8a3e8de162a4500e6e8b6b95f65a992ce
Nguyễn Thành Thức <nguyenthanhthuc.2k@gmail.com>
2022-03-05 08:19:14 +0700

    add

--- membersite/views/pages/method.php
+++ membersite/views/pages/method.php
@@ -3,7 +3,7 @@
 
     <nav aria-label="Page navigation example">
         <ul class="pagination">
-            <li class="page-item">
+            <li class="page-item active">
                 <a class="page-link" href="#" aria-label="Previous">
                     <span aria-hidden="true">&laquo;</span>
                 </a>
@@ -11,6 +11,9 @@
             <li class="page-item"><a class="page-link" href="#">1</a></li>
             <li class="page-item"><a class="page-link" href="#">2</a></li>
             <li class="page-item"><a class="page-link" href="#">3</a></li>
+            <li class="page-item"><a class="page-link" href="#">4</a></li>
+            <li class="page-item"><a class="page-link" href="#">5</a></li>
+            <li class="page-item"><a class="page-link" href="#">6</a></li>
             <li class="page-item">
                 <a class="page-link" href="#" aria-label="Next">
                     <span aria-hidden="true">&raquo;</span>
